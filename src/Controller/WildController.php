2a7fbab11e3b1bef17aad561d9d4ffc5a6725d67
ahmad <ahmadsafar257@gmail.com>
2020-05-21 18:45:44 +0200

    quete8

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,8 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,32 +11,87 @@ use Symfony\Component\Routing\Annotation\Route;
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+    * Show all rows from Program’s entity
+    *
+    *@Route("/wild", name="wild_index")
+    *@return Response A response instance
     */
     public function index() :Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
-    ]);
+        $programs=$this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findAll();
+        if (!$programs)  {
+            throw $this->createNotFoundException(
+            'No program found in program\'s table.'
+            );
+        }
+        return $this->render('wild/index.html.twig', ['programs' => $programs]);
     }
-     /**
-    *@Route("wild/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="wild_show")
-     * @param string $slug The slugger
-     * @return Response
-   */
-   public function show(?string $slug): Response
-   {
-   
-    if (!$slug) {
-        $slug = "Aucune série sélectionnée, veuillez choisir une série";
-    }
-    $slug = preg_replace(
-        '/-/',
-        ' ', ucwords(trim(strip_tags($slug)), "-")
-    );
 
-    return $this->render('wild/show.html.twig', [
-        'slug' => $slug,
-    ]);
-   }
+    /**
+    * Getting a program with a formatted slug for title
+    *
+    * @param string $slug The slugger
+    * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="wild_show")
+    * @return Response
+    */
+    public function show(?string $slug):Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'program' => $program,
+            'slug'  => $slug,
+        ]);
+    }
+
+    /**
+     * @param string $categoryName
+     * @Route("/category/{categoryName}", name="show_category")
+     * @return Response
+     */
+    public function showByCategory(string $categoryName):Response
+    {
+        if(!$categoryName){
+            throw $this
+                  ->createNotFoundException('No category Name has been sent to find a program in program\'s table.');
+        }
+        $categoryName=preg_replace(
+            '/-/',
+            ' ',ucwords(trim(strip_tags($categoryName)),"-")
+        );
+        $category=$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name'=> mb_strtolower($categoryName)]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category' => $category], ['id' => 'desc'], 3);
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No programs with '.$categoryName.' category found in program\'s table'
+            );
+        }
+            return $this->render('wild/category.html.twig', [
+                'programs' => $programs,
+                'category' => $category,
+            ]);
+      
+    }
 }
