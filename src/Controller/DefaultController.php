06080a42095f27bd330fcf4148f83fc5f64a2a83
ahmad <ahmadsafar257@gmail.com>
2020-05-25 15:23:59 +0200

    Q10

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,6 +15,6 @@ class DefaultController extends AbstractController
      */
     public function index() :Response
     {
-        return $this->render('wild/home.html.twig');
+        return $this->render('base.html.twig');
     }
 }
